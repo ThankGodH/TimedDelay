3ab8cd6675ef5a9c943e57a6eaaae3cb47b0a233
ThankGodH <n01754713@humber.ca>
2026-03-13 23:57:23 -0400

    Update timedDelayNothreads.c

--- timedDelayNothreads.c
+++ timedDelayNothreads.c
@@ -7,78 +7,83 @@ Multithreaded TCP server that handles multiple clients concurrently
 using pthreads. Each client request is processed in its own thread.
 */
 
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#ifdef _WIN32
 #include <winsock2.h>
-#include <ws2tcpip.h>
-#define sleep(x) Sleep(1000 * (x))
-#else
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#endif
+#include <windows.h>
+#include <pthread.h>
 
 #define PORT 8080
 
-void handle_client(SOCKET client_socket, int client_id) {
-    char *message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\nConnection: close\r\n\r\nHello Client!";
-    
-    printf("[Server] Handling client %d...\n", client_id);
-    
-    // Simulating "bottleneck" or heavy processing
-    printf("[Server] Processing request for 5 seconds...\n");
-    sleep(5); 
-    
-    send(client_socket, message, (int)strlen(message), 0);
-    printf("[Server] Response sent to client %d. Closing connection.\n", client_id);
-    
-#ifdef _WIN32
+// Function that handles each client connection
+void* handle_client(void* arg)
+{
+    SOCKET client_socket = *(SOCKET*)arg;
+    free(arg);
+
+    printf("Client connected\n");
+
+    // Simulate server work
+    Sleep(5000);
+
+    printf("Finished processing client\n");
+
     closesocket(client_socket);
-#else
-    close(client_socket);
-#endif
+
+    return NULL;
 }
 
-int main() {
-#ifdef _WIN32
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-#endif
+int main()
+{
+    WSADATA wsa;
+    SOCKET server_socket, *client_socket;
+    struct sockaddr_in server, client;
+    int c;
 
-    SOCKET server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    int addr_len = sizeof(client_addr);
-    int client_count = 0;
+    printf("Initializing Winsock...\n");
+    WSAStartup(MAKEWORD(2,2), &wsa);
 
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    server_socket = socket(AF_INET , SOCK_STREAM , 0 );
 
-    bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    listen(server_socket, 5);
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
 
-    printf("Server listening on port %d...\n", PORT);
-    printf("NOTE: This server is SEQUENTIAL. It can only handle one client at a time!\n\n");
+    bind(server_socket, (struct sockaddr *)&server , sizeof(server));
 
-    while (1) {
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
-        if (client_socket != INVALID_SOCKET) {
-            client_count++;
-            // This is the sequential bottleneck:
-            handle_client(client_socket, client_count);
+    listen(server_socket , 5);
+
+    printf("Server waiting for connections...\n");
+
+    c = sizeof(struct sockaddr_in);
+
+    while((client_socket = malloc(sizeof(SOCKET))))
+    {
+        *client_socket = accept(server_socket ,
+            (struct sockaddr *)&client, &c);
+
+        if (*client_socket < 0)
+        {
+            perror("accept failed");
+            return 1;
         }
+
+        pthread_t thread_id;
+
+        // Create new thread for client
+        if (pthread_create(&thread_id , NULL , handle_client ,
+            (void*)client_socket) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+
+        // Automatically free thread resources
+        pthread_detach(thread_id);
     }
 
-#ifdef _WIN32
     closesocket(server_socket);
     WSACleanup();
-#else
-    close(server_socket);
-#endif
+
     return 0;
 }
